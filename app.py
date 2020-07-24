59d861e7c6fe4d30c0dcdb6d55e5f82ab8a4db65
Sanket Dhole <sanket.dhole1997@gmail.com>
2020-07-24 10:26:48 +0530

    Initial Working Server setup for Web Visitor

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import request, jsonify
+import time
+import json
+
+from util import ImageDownloader
+
+imageDownloader = ImageDownloader()
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    imageDownloader.removeOldFiles()
+    return json.dumps({
+        'name':'Web Visitor',
+        'status': 'Active',
+        'time':time.ctime()
+        })
+
+@app.route("/image", methods=["POST"])
+def getImage():
+    imageDownloader.removeOldFiles()
+    if request.method == "POST":
+        return imageDownloader.addUrl(request.form['url'])
+            
+
+
+if __name__=="__main__":
+    app.run()
---
+++ util.py
@@ -0,0 +1,59 @@
+from datetime import datetime
+import json
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+
+WINDOW = "1920,1080"
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--window-size=%s' %WINDOW)
+
+headless_browsers = []
+chrome_instances = 5
+
+for i in range(chrome_instances):
+    driver= webdriver.Chrome(chrome_options=chrome_options)
+    headless_browsers.append(driver)
+
+
+class ImageDownloader:
+    """
+        ImageDownloader class will create chrome brower instances.
+        It will load the webpages from the urls list and save a ss in temp folder
+    """
+    def __init__(self):
+        self.urlsMapper = {}
+        self.count = 0
+        self.mapper = {}
+
+    def addUrl(self, url):
+        filename =  datetime.now().isoformat()+".png"
+        self.mapper[filename] = False
+        self.urlsMapper[filename] = url
+        res = self.getImage(filename)
+        if(res == "error"):
+            return res
+        return "/static/temp/"+filename
+    
+    def getImage(self, filename):
+        driver = headless_browsers[self.count % chrome_instances]
+        self.count+=1
+        current_url = self.urlsMapper[filename]
+        if not current_url.startswith('http'):
+            return 'error'
+        driver.get(current_url)
+        driver.save_screenshot(os.path.join('static','temp',filename))
+        self.mapper[filename] = True
+        return True
+
+    def removeOldFiles(self):
+        currentTimeStamp = datetime.now().timestamp()
+        with os.scandir('static/temp') as s:
+            for file in s:
+                lastModified = os.path.getmtime(file)
+                diff = currentTimeStamp - lastModified
+                if((diff/(24*60*60)) > 1):
+                    os.remove(file)
+            
